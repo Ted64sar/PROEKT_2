4de77b0eda4bd4f3b02a94ed212208cad1056bbf
Ted64sar <fedor-commander@yandex.ru>
2019-02-05 22:21:54 +0400

    Тренируюсь с картинками, чтобы понять, как рисовать

---
+++ LOGIKA_IGRI.py
@@ -0,0 +1,51 @@
+import pygame
+import os
+
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, 80, 80)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    return image
+
+
+screen = pygame.display.set_mode((80, 80))
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+dragon = AnimatedSprite(load_image("XOLST.png"), 8, 2, 0, 0)
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        # при закрытии окна
+        if event.type == pygame.QUIT:
+            running = False
+    dragon.update()
+    all_sprites.draw(screen)
+    clock.tick(1)
+
+    pygame.display.flip()
