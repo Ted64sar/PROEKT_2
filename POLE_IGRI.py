3c2fa2637258cdaf210a2be8dbcdafed505078df
Ted64sar <fedor-commander@yandex.ru>
2019-02-10 21:44:02 +0400

    Делай зомби

--- POLE_IGRI.py
+++ POLE_IGRI.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 size = width, height = 1000, 580
@@ -18,21 +19,28 @@ class Pole:
         self.columns = columns
         self.rows = rows
         self.board = [[0] * columns for _ in range(rows)]
+        self.image = load_image('POLE_IGRY.png')
         # значения по умолчанию
         self.x = 100
         self.y = 50
         self.cell_size = 80
 
     def render(self):
-        for i in range(self.rows):
-            for j in range(self.columns):
-                pygame.draw.rect(screen, (255, 255, 255),
-                                 [self.x + j * self.cell_size, self.y + i * self.cell_size, self.cell_size, self.cell_size], 1)
+        image1 = pygame.transform.scale(self.image, (1000, 580))
+        screen.blit(image1, (0, 0))
 
 
 
 
 
+def load_image(name):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    return image
 board = Pole(10, 6)
 
 running = True
